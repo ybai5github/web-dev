d0c38f2a164559552008cb5c0ae5a70821aa617c
Ye Bai <ybai5@binghamton.edu>
2022-04-04 13:28:57 -0400

    a8 done

--- src/components/tuiter/services/tuits-service.js
+++ src/components/tuiter/services/tuits-service.js
@@ -1,5 +1,9 @@
 import axios from 'axios';
-const TUITS_API = 'http://localhost:4000/api/tuits';
+// const TUITS_API = 'http://localhost:4000/api/tuits';
+// const TUITS_API = 'https://full-stack-developer-server.herokuapp.com/api/tuits';
+
+const API_BASE = process.env.REACT_APP_API_BASE;
+const TUITS_API = `${API_BASE}/tuits`;
 
 
 export const findAllTuits = async () => {
