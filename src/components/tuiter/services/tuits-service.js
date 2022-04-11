afb7a3023e1303b4e1ae29a2a18390e706eaa42c
Ye Bai <ybai5@binghamton.edu>
2022-04-11 14:22:25 -0400

    a9 done

--- src/components/tuiter/services/tuits-service.js
+++ src/components/tuiter/services/tuits-service.js
@@ -2,7 +2,8 @@ import axios from 'axios';
 // const TUITS_API = 'http://localhost:4000/api/tuits';
 // const TUITS_API = 'https://full-stack-developer-server.herokuapp.com/api/tuits';
 // set REACT_APP_API_BASE in netlify's setting
-const API_BASE = process.env.REACT_APP_API_BASE;
+// const API_BASE = process.env.REACT_APP_API_BASE;
+const API_BASE = 'http://localhost:4000/api';
 const TUITS_API = `${API_BASE}/tuits`;
 
 
